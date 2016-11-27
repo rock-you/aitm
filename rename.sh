508460f6775472f1d982bcca7954b06bcbdc8b74
CharlesTBetz <char@erp4it.com>
2016-11-27 09:16:21 -0600

    commit

--- rename.sh
+++ rename.sh
@@ -1,14 +1,9 @@
-# renaming file names
-rename -v -n 's/first dot/underscore/' *.png
+# renaming file names from #.##-title.jpg|png to #_##-title.png|png
 
-
-  rename -v -n 's/(\d)\.(\d\d)/$1_$2/' *.png
+rename -v -n 's/(\d)\.(\d\d)/$1_$2/' *.png
 
 # then same thing within files but has to match *.png or *.jpg with no space
 #1.01-blabla.jpg or .png
 #(\b\d).(\d\d*.png) /$1_$2
 
-sed -E -e 's/([0-9])\.([0-9]{2}.*[png|jpg])/\1_\2/g' test.adoc
-
-sed -E 's/([0-9])\.([0-9]{2}.*[png|jpg])/\1_\2/g' *.adoc
-sed -E -i bak 's/([0-9])\.([0-9]{2}.*[png|jpg])/\1_\2/g' *.adoc
+sed -E -i .bak 's/([0-9])\.([0-9]{2}.*[png|jpg])/\1_\2/g' *.adoc
