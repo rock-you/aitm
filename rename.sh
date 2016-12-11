a9a6117e9663e408af369d017743d77d5333917f
Charles T. Betz <char@erp4it.com>
2016-12-11 09:52:17 -0600

    commit

--- rename.sh
+++ rename.sh
@@ -2,8 +2,17 @@
 
 rename -v -n 's/(\d)\.(\d\d)/$1_$2/' *g
 
+rename -v -n 's/3_09(.*)/5_04$1/' *.png
+
+
+
+
 # then same thing within files but has to match *.png or *.jpg with no space
 #1.01-blabla.jpg or .png
 #(\b\d).(\d\d*.png) /$1_$2
 
 sed -E -i .bak 's/([0-9])\.([0-9]{2}.*[png|jpg])/\1_\2/g' *.adoc
+
+sed -E -i  's/3_09/5_04/g' *process.adoc
+
+note: .bak is mac os variant
