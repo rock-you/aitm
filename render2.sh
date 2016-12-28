5f7a1995d086a8470e1e1ba3d3e4b5f665c465c3
CharlesTBetz <char@erp4it.com>
2016-12-27 18:18:27 -0600

    commit

--- render2.sh
+++ render2.sh
@@ -3,4 +3,15 @@
 # experiment 1: render asciidoc from pdf
 #asciidoctor-pdf -a allow-uri-read aitm-collaborator-pdf.adoc
 
-asciidoctor-pdf aitm-instructor-pdf.adoc 
+asciidoctor-pdf aitm-instructor-pdf.adoc
+
+# experiment 2: asciidoc to docbook to latex to pdf
+
+asciidoctor -b docbook aitm-instructor-pdf.adoc
+
+# converts to pdf via latex
+dblatex aitm-instructor-pdf.xml
+
+
+# converst to latex only
+dblatex -t tex aitm-instructor-pdf.xml
