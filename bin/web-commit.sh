9945235cdffd4f8e1acc8dccda04151d24b1111f
CharlesTBetz <char@erp4it.com>
2017-01-19 07:29:30 -0600

    commit

--- bin/web-commit.sh
+++ bin/web-commit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-cp ~/repos/aitm/web/*.html ~/repos/aitm-pages
+cp ~/repos/aitm/web/*.html ~/repos/aitm-pages/test/
+cp ~/repos/aitm/web/*.js ~/repos/aitm-pages/test
+
+
+cd ~/repos/aitm-pages
 
 git add . -A && git commit -m "commit"
+
 git push origin gh-pages
